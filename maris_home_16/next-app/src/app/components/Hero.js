ba6f7dc73ef901edc6255d8c13aa661d9eaba88d
Maris-Pidiks <maris@divikiwi.lv>
2025-02-26 16:32:30 +0200

    Update Hero.js

--- maris_home_16/next-app/src/app/components/Hero.js
+++ maris_home_16/next-app/src/app/components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">maris_home_16</h1>
-          <p className="text-2xl py-6">This is my first two next.js projects.</p>
+          <p className="text-2xl py-6">This is my first next.js project.</p>
           <Link href="/pages/Githubusers" className="btn btn-success text-white">
             Projects
           </Link>
