9b85e2679a7d39c8e0881456e421ad810353835c
Māris Pidiks <maris@divikiwi.lv>
2025-03-20 07:42:54 +0200

    change eslinter config

--- maris_home_16/next-app/eslint.config.mjs
+++ maris_home_16/next-app/eslint.config.mjs
@@ -1,14 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
-
-export default eslintConfig;
+export default [
+  {
+    plugins: {
+      react: reactPlugin,
+      next: nextPlugin,
+      "react-hooks": hooksPlugin,
+    },
+    extends: [
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "plugin:@next/next/recommended",
+    ],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
+    },
+    env: {
+      browser: true,
+      node: true,
+      es6: true,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+  },
+];
