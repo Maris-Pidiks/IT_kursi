82c0d751246e807038869acc01e4ca36938bf452
DiviKiwi <maris.pidiks.24@gmail.com>
2025-04-09 10:43:33 +0300

    n

--- maris_home_16/next-app/src/app/components/NotFound.js
+++ maris_home_16/next-app/src/app/components/NotFound.js
@@ -6,14 +6,16 @@ export default function NotFound() {
   return (
     <div className="container w-full max-w-full mx-auto p-4 flex justify-center min-h-screen bg-base-200 px-5 md:px-20">
       <div className="container max-w-3xl mx-auto p-4 text-center">
-        <div className="card bg-base-100 shadow-xl p-8">
-          <h2 className="text-4xl font-bold mb-4">Post Not Found</h2>
+        <div className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow duration-200 p-8">
+          <h2 className="text-4xl font-bold mb-4">Not Found</h2>
           <p className="text-gray-600 mb-8">
-            Sorry, we could not find the post you are looking for.
+            Sorry, we could not find what you are looking for.
           </p>
-          <Link href="/blog" className="btn btn-success btn-sm text-white">
-            ← Back to Blog
-          </Link>
+          <div className="flex justify-center">
+            <Link href="/blog" className="btn btn-success btn-sm text-white">
+              ← Back to Blog
+            </Link>
+          </div>
         </div>
       </div>
     </div>
